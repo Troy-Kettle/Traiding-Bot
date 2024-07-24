969d59e3db271ccf1d596a84976de325a17b6222
Troy Kettle <Troy.Kettle@nottingham.ac.uk>
2024-07-24 12:38:17 +0100

    Building of stratergy

---
+++ tradingbot.py
@@ -0,0 +1,38 @@
+from lumibot.brokers import Alpaca
+from lumibit.backtesting import YahooDataBacktesting
+from lumibot.strategies import Strategy
+from lumibot.traders import Trader
+
+from datetime import datetime
+
+API_KEY = 'CKM558QI1SLPKKS1H2HK'
+API_SECRET = 'CK9F9QE7XZ9I69VXAJ41'
+BASE_URL = 'https://paper-api.alpaca.markets'
+
+ALAPACA_CREDS = {
+    'API_KEY': API_KEY,
+    'API_SECRET': API_SECRET,
+    'PAPER': True
+}
+
+class MLTrader(Strategy):
+    def initialise(self, symbol:str="SPY"):
+        self.symbol = symbol
+        self.sleeptime = "24H"
+        self.last_trade = None
+    def on_trading_iteration(self):
+        if self.last_trade == None:
+            order = self.create_order(self.symbol, 10, "buy", type="market")
+
+
+start_date = datetime(2023, 15, 1)
+end_date = datetime(2023, 15, 31)
+
+broker = Alpaca(ALAPACA_CREDS)
+strategy = MLTrader(name='mlstrat', broker=broker,
+                    parameters={"symbol": "SPY"})
+strategy.backtest(YahooDataBacktesting,
+                  start_date,
+                  end_date,
+                  paramters={})
+
