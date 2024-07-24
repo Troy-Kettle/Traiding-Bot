a35a0e3354726d38faa2352ae14b6e7ea6b85e5f
Troy Kettle <Troy.Kettle@nottingham.ac.uk>
2024-07-24 12:47:09 +0100

    Getting ssl error need to fix this asap

--- tradingbot.py
+++ tradingbot.py
@@ -1,12 +1,12 @@
 from lumibot.brokers import Alpaca
-from lumibit.backtesting import YahooDataBacktesting
+from lumibot.backtesting import YahooDataBacktesting
 from lumibot.strategies import Strategy
 from lumibot.traders import Trader
 
 from datetime import datetime
 
-API_KEY = 'CKM558QI1SLPKKS1H2HK'
-API_SECRET = 'CK9F9QE7XZ9I69VXAJ41'
+API_KEY = 'CK9F9QE7XZ9I69VXAJ41'
+API_SECRET = 'UgUy7R48JmQAR8RDZb3zTVji1IbGb0KrqhEgDT0r'
 BASE_URL = 'https://paper-api.alpaca.markets'
 
 ALAPACA_CREDS = {
@@ -23,10 +23,12 @@ class MLTrader(Strategy):
     def on_trading_iteration(self):
         if self.last_trade == None:
             order = self.create_order(self.symbol, 10, "buy", type="market")
+            self.submit_order(order)
+            self.last_trade = "buy"
 
 
-start_date = datetime(2023, 15, 1)
-end_date = datetime(2023, 15, 31)
+start_date = datetime(2023, 12, 1)
+end_date = datetime(2023, 12, 31)
 
 broker = Alpaca(ALAPACA_CREDS)
 strategy = MLTrader(name='mlstrat', broker=broker,
